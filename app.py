4bfd2e56673a0613c3cfad6b51a168ce7f366a15
Keshav Nayak <keshav@r2d2tech.in>
2020-03-09 16:23:56 +0530

    Query_Update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Body
+from fastapi import FastAPI, Body, Query
 from Database.database import Database
 from fastapi.encoders import jsonable_encoder
 from Database.post import Book
@@ -21,28 +21,31 @@ async def get_book_by_isbn(isbn:int):
         return 'book with isbn {} does not exist'.format(isbn)
     else:
         books['_id'] =  str(books['_id'])
-        return jsonable_encoder(books)
+        return books
 
 @app.post("/books")
 async def add_book_to_mongo(books: List[validation.Add_Book] =Body(...)):
     for book in books:
         book_dict = book.dict()
-        isbn=book_dict['isbn']
-        books = await get_book_by_isbn(int(isbn))
-        if books == 'book with isbn {} does not exist'.format(isbn):
+        books = await get_book_by_isbn(int(book_dict['isbn']))
+        if books == 'book with isbn {} does not exist'.format(book_dict['isbn']):
             book_dict.update({'date_of_creation':datetime.datetime.now()})
             mongo_book.add_to_mongo_directly(book_dict)
-            return await get_book_by_isbn(int(isbn))
+            return await get_book_by_isbn(int(book_dict['isbn']))
         else:
             return 'book with isbn {} already exists'.format(int(isbn))
 
 @app.patch('/books/{isbn}')
-async def update_mongo(isbn:int, book:validation.Update_Book = Body(...)):
+async def update_mongo(isbn:int, price:int = Query(None, description='enter the price'), name:str= Query(None)):
     books = await get_book_by_isbn(isbn)
     if books == 'book with isbn {} does not exist'.format(isbn):
         return books
+    elif name==None:
+        update = {'price':price,'name':books['name']}
+        mongo_book.update_mongo_book(query=isbn, updates=update)
+        return await get_book_by_isbn(int(isbn))
     else:
-        update = book.dict()
+        update = {'price': price, 'name': name}
         mongo_book.update_mongo_book(query=isbn, updates=update)
         return await get_book_by_isbn(int(isbn))
 
